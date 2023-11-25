29881acaaaeaafc840acf62cac35b41084cd08d6
Simone-Corso <scorso26@gmail.com>
2023-11-25 16:30:20 +0100

    inserito la variabile per le difficoltà del gioco.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@
 //variabile per il button :
     const nuovaPartitaEl = document.querySelector('button#button-inizia');
     console.log(nuovaPartitaEl);
+
+//variabile per le difficoltà del gioco
+    const difficoltàGioco = document.querySelector('select#difficoltà-gioco');
+    console.log(difficoltàGioco);
