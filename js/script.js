b3f6027ed5ed87ef910ceed87f3b1605fa36cf16
Simone-Corso <scorso26@gmail.com>
2023-11-25 16:27:10 +0100

    inserito tutto in modo corretto

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@
 
     const mainContenutoEl = document.querySelector('main section.main-content');
     console.log(mainContenutoEl);
-
+//variabile per il button :
+    const nuovaPartitaEl = document.querySelector('button#button-inizia');
+    console.log(nuovaPartitaEl);
