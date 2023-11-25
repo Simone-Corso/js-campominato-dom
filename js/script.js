6ccdccb7a02bc61096ff6ec98360111360e7c3b3
Simone-Corso <scorso26@gmail.com>
2023-11-25 16:16:50 +0100

    errore di file, ricomincio

--- js/script.js
+++
@@ -1,2 +0,0 @@
-//Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
-//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
